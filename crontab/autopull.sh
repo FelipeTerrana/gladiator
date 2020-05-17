8261c92e2a48397ef5672126e390f7a0c319f090
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2020-05-17 16:10:31 -0300

    Rearrange docker folder

--- crontab/autopull.sh
+++ crontab/autopull.sh
@@ -3,7 +3,7 @@
 eval $(ssh-agent -s)
 ssh-add
 git pull origin master
-crontab docker/crontabfile
+crontab crontab/crontabfile
 cp docker/id_rsa /root/.ssh/
 cp docker/id_rsa.pub /root/.ssh/
 cp docker/known_hosts /root/.ssh/
